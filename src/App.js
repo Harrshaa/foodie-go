eccb253f2561c66257ca39395e3aba319f7cf2bb
Sri Harsha Mutakodoor <sriharsha.mutakodoor@gmail.com>
2025-08-22 14:09:08 +0530

    redux store initialization

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ import RestaurantMenu from "./components/RestaurantMenu";
 // import Grocery from "./components/Grocery";
 import UserContext from "./utils/UserContext";
 import { createBrowserRouter,Outlet,RouterProvider } from "react-router-dom";
-import UserContext from "./utils/UserContext";
 
-import appStore from "./utils/appStore";
+
+
 
 const Grocery = lazy(()=>{
 
@@ -21,16 +21,16 @@ const App =()=>{
    
 
     return(
-        <Provider store={appStore}>
-        <UserContext.Provider value={{loggedInUser:"Harsha"}}>
+   
+   
         <div className="app">
-        <UserContext.Provider value={{loggedInUser: "Harrshaa"}}>
+       
         <HeaderComponent/>
-        </UserContext.Provider>
+    
         <Outlet/>
         </div>
-        </UserContext.Provider>
-        </Provider>
+
+
     )
 }
 
